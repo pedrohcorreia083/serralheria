bd9003b1cb46ef385b0fb07aac528b45fba2029a
pedrohcorreia083 <pedrohcorreia083@gmail.com>
2025-09-02 15:58:22 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Strategy
-* Template URL: https://bootstrapmade.com/strategy-bootstrap-agency-template/
-* Updated: May 09 2025 with Bootstrap v5.3.6
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
